65929720aa1ec6b5b698a571631ac7c23719ae0e
Alex Muramoto <amuramoto@apache.org>
2018-02-01 18:00:08 -0800

    Adds basic test runner

--- run.js
+++ run.js
@@ -1,26 +1,58 @@
-const Event = require('./lib/index.js');
+const test = require('./lib');
+let options = {
+  timestamp: new Date().getTime(),
+  page_id: '44444444',
+  sender: {'id': '33333333'},        
+  mid: "mid.$cAAJsujCd2ORnUSd1SFhH-IsSityC",
+  title: "TITLE",
+  payload: "PAYLOAD",
+  referral_param: "REFERRAL_PARAM",
+  watermark: 1458668856253,
+  text: "TEXT",
+  ad_id: "AD_ID",
+  metadata: "METADATA",
+  seq: 37,
+  user_ref: "USER_REF",
+  street_1: '1 Hacker Way',
+  street_2: 'Building 15',
+  city: 'MENLO PARK',
+  state: 'CA',
+  country: 'US',
+  postal_code: '94025',
+  contact_name: 'Peter Chang',
+  contact_email: 'peter@anemailprovider.com',
+  contact_phone: '+15105551234',    
+  provider_type: 'stripe',
+  charge_id: 'ch_18tmdBEoNIH3FPJHa60ep123',
+  fb_payment_id: '123456789',        
+  shipping_option_id: '123',
+  app_id: '235408346',
+  authorization_code: 'AUTHORIZATION_CODE',
+}
 
-console.log(JSON.stringify(new Event('messages'), null, 2))
-console.log(JSON.stringify(new Event('messaging_postbacks'), null, 2))
-console.log(JSON.stringify(new Event('messaging_postbacks.m_me'), null, 2))
-console.log(JSON.stringify(new Event('messaging_postbacks.ad_referrals'), null, 2))
-console.log(JSON.stringify(new Event('messaging_postbacks.messenger_code'), null, 2))
-console.log(JSON.stringify(new Event('messaging_postbacks.discover_tab'), null, 2))
-console.log(JSON.stringify(new Event('messaging_referrals.m_me'), null, 2))
-console.log(JSON.stringify(new Event('messaging_referrals.ad_referrals'), null, 2))
-console.log(JSON.stringify(new Event('messaging_referrals.messenger_code'), null, 2))
-console.log(JSON.stringify(new Event('messaging_referrals.discover_tab'), null, 2))
-console.log(JSON.stringify(new Event('message_echoes'), null, 2))
-console.log(JSON.stringify(new Event('message_deliveries'), null, 2))
-console.log(JSON.stringify(new Event('message_reads'), null, 2))
-console.log(JSON.stringify(new Event('messaging_checkout_updates'), null, 2))
-console.log(JSON.stringify(new Event('messaging_pre_checkouts'), null, 2))
-console.log(JSON.stringify(new Event('messaging_handovers.pass_thread_control'), null, 2))
-console.log(JSON.stringify(new Event('messaging_handovers.take_thread_control'), null, 2))
-console.log(JSON.stringify(new Event('messaging_handovers.request_thread_control'), null, 2))
-console.log(JSON.stringify(new Event('messaging_handovers.app_roles'), null, 2))
-console.log(JSON.stringify(new Event('messaging_payments'), null, 2))
-console.log(JSON.stringify(new Event('messaging_policy_enforcement'), null, 2))
-console.log(JSON.stringify(new Event('messaging_optins'), null, 2))
-console.log(JSON.stringify(new Event('messaging_account_linking.linked'), null, 2))
-console.log(JSON.stringify(new Event('messaging_account_linking.unlinked'), null, 2))
+// console.log(JSON.stringify(new test.WebhookEvent('messages.text', options), null, 2))
+// console.log(JSON.stringify(new test.WebhookEvent('messaging_postbacks', options), null, 2))
+// console.log(JSON.stringify(new test.WebhookEvent('messaging_postbacks.m_me', options), null, 2))
+// console.log(JSON.stringify(new test.WebhookEvent('messaging_postbacks.ad_referrals', options), null, 2))
+// console.log(JSON.stringify(new test.WebhookEvent('messaging_postbacks.messenger_code', options), null, 2))
+// console.log(JSON.stringify(new test.WebhookEvent('messaging_postbacks.discover_tab', options), null, 2))
+// console.log(JSON.stringify(new test.WebhookEvent('messaging_referrals.m_me', options), null, 2))
+// console.log(JSON.stringify(new test.WebhookEvent('messaging_referrals.ad_referrals', options), null, 2))
+// console.log(JSON.stringify(new test.WebhookEvent('messaging_referrals.messenger_code', options), null, 2))
+// console.log(JSON.stringify(new test.WebhookEvent('messaging_referrals.discover_tab', options), null, 2))
+// console.log(JSON.stringify(new test.WebhookEvent('message_echoes', options), null, 2))
+// console.log(JSON.stringify(new test.WebhookEvent('message_deliveries', options), null, 2))
+// console.log(JSON.stringify(new test.WebhookEvent('message_reads', options), null, 2))
+// console.log(JSON.stringify(new test.WebhookEvent('messaging_checkout_updates', options), null, 2))
+// console.log(JSON.stringify(new test.WebhookEvent('messaging_pre_checkouts', options), null, 2))
+// console.log(JSON.stringify(new test.WebhookEvent('messaging_handovers.pass_thread_control', options), null, 2))
+// console.log(JSON.stringify(new test.WebhookEvent('messaging_handovers.take_thread_control', options), null, 2))
+// console.log(JSON.stringify(new test.WebhookEvent('messaging_handovers.request_thread_control', options), null, 2))
+// console.log(JSON.stringify(new test.WebhookEvent('messaging_handovers.app_roles', options), null, 2))
+// console.log(JSON.stringify(new test.WebhookEvent('messaging_payments', options), null, 2))
+// console.log(JSON.stringify(new test.WebhookEvent('messaging_policy_enforcement', options), null, 2))
+// console.log(JSON.stringify(new test.WebhookEvent('messaging_optins', options), null, 2))
+// console.log(JSON.stringify(new test.WebhookEvent('messaging_account_linking.linked', options), null, 2))
+// console.log(JSON.stringify(new test.WebhookEvent('messaging_account_linking.unlinked', options), null, 2))
+// 
+test.send('ok')
